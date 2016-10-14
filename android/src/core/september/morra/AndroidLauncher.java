8b3a0140c8080cc99938afbfc072c01ae97449fd
theswolf <chris.geymo@gmail.com>
2016-10-14 16:06:10 +0200

    first beta

--- android/src/core/september/morra/AndroidLauncher.java
+++ android/src/core/september/morra/AndroidLauncher.java
@@ -1,15 +1,26 @@
 package core.september.morra;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.RelativeLayout;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.backends.android.AndroidApplication;
 import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdSize;
+import com.google.android.gms.ads.AdView;
 import com.google.android.gms.games.Games;
 import com.google.example.games.basegameutils.GameHelper;
+
+import android.widget.RelativeLayout.LayoutParams;
 
 import core.september.morra.MorraGame;
 import core.september.morra.util.PlayServices;
@@ -18,9 +29,11 @@ public class AndroidLauncher extends AndroidApplication implements PlayServices 
 
 	private GameHelper gameHelper;
 	private final static int requestCode = 1;
+	private AdView adView;
+	private View gameView;
 
-	@Override
-	protected void onCreate (Bundle savedInstanceState) {
+	//@Override
+	protected void onCreateOld (Bundle savedInstanceState) {
 
 		StrictMode.setVmPolicy(new StrictMode.VmPolicy.Builder().detectAll().penaltyLog()
 				.penaltyDeath().build());
@@ -43,6 +56,76 @@ public class AndroidLauncher extends AndroidApplication implements PlayServices 
 		gameHelper.setup(gameHelperListener);
 
 		initialize(new MorraGame(this), config);
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		AndroidApplicationConfiguration cfg = new AndroidApplicationConfiguration();
+		//cfg.useGL20 = false;
+		cfg.useAccelerometer = false;
+		cfg.useCompass = false;
+
+		// Do the stuff that initialize() would do for you
+		requestWindowFeature(Window.FEATURE_NO_TITLE);
+		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+		getWindow().clearFlags(WindowManager.LayoutParams.FLAG_FORCE_NOT_FULLSCREEN);
+
+		RelativeLayout layout = new RelativeLayout(this);
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.MATCH_PARENT, RelativeLayout.LayoutParams.MATCH_PARENT);
+		layout.setLayoutParams(params);
+
+		AdView admobView = createAdView();
+		layout.addView(admobView);
+		View gameView = createGameView(cfg);
+		layout.addView(gameView);
+
+		setContentView(layout);
+		startAdvertising(admobView);
+	}
+
+	private void startAdvertising(AdView adView) {
+		AdRequest adRequest = new AdRequest.Builder().build();
+		adView.loadAd(adRequest);
+	}
+
+	private AdView createAdView() {
+		adView = new AdView(this);
+		adView.setAdSize(AdSize.SMART_BANNER);
+		adView.setAdUnitId(getString(R.string.banner_ad_unit_id));
+		adView.setId(R.id.adviewId); //n arbitrary id, allows for relative positioning in createGameView()
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+		params.addRule(RelativeLayout.ALIGN_PARENT_TOP, RelativeLayout.TRUE);
+		params.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+		adView.setLayoutParams(params);
+		adView.setBackgroundColor(Color.BLACK);
+		return adView;
+	}
+
+	private View createGameView(AndroidApplicationConfiguration cfg) {
+
+		gameHelper = new GameHelper(this, GameHelper.CLIENT_GAMES);
+		gameHelper.enableDebugLog(false);
+
+		GameHelper.GameHelperListener gameHelperListener = new GameHelper.GameHelperListener()
+		{
+			@Override
+			public void onSignInFailed(){ }
+
+			@Override
+			public void onSignInSucceeded(){ }
+		};
+
+		gameHelper.setup(gameHelperListener);
+
+		gameView = initializeForView(new MorraGame(this), cfg);
+		RelativeLayout.LayoutParams params = new RelativeLayout.LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+		params.addRule(RelativeLayout.ALIGN_PARENT_BOTTOM, RelativeLayout.TRUE);
+		params.addRule(RelativeLayout.CENTER_HORIZONTAL, RelativeLayout.TRUE);
+		params.addRule(RelativeLayout.BELOW, adView.getId());
+		gameView.setLayoutParams(params);
+		return gameView;
 	}
 
 	@Override
